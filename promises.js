fc6e4a7a74f0d722c4e2dcaa1390d7ce6ddc978f
mugisha thicien <mugishathicien04@gmail.com>
2025-10-15 17:33:10 +0200

    okay

--- promises.js
+++ promises.js
@@ -1,1 +1,10 @@
-function login
+function login(password) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            password: ("1234")
+                ? ("login successfully!")
+                : ("access denied!");
+        }, 2000);
+    });
+}
+login("1234").then(console.log).catch(console.log);
