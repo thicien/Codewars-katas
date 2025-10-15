76fc804198bca24870e15e3a4bde46ce6b5bc9fd
mugisha thicien <mugishathicien04@gmail.com>
2025-10-15 13:40:12 +0200

    solved

--- string.js
+++ string.js
@@ -1,28 +1,25 @@
-function solve(s) {
-  let stack = [];
-
-  for (let char of s) {
-    if (char !== ')') {
+function solve(str){
+let stack = [];
+  for(let char of str) {
+    if(char !== ')'){
       stack.push(char);
     } else {
       let segment = '';
-      while (stack[stack.length - 1] !== '(') {
+      while(stack([stack.length - 1 !== '('])){
         segment = stack.pop() + segment;
       }
       stack.pop();
       let num = '';
-      while (stack.length && !isNaN(stack[stack.length - 1])) {
+      while(stack.length && isNaN(stack[stack.length - 1])){
         num = stack.pop() + num;
       }
       num = Number(num);
-      let expanded = segment.repeat(num);
-      stack.push(expanded);
+      let enlarged = segment.repeat(num);
+      num.push(enlarged);
     }
   }
   return stack.join('');
 }
-console.log(solve("3(ab)"));        
-console.log(solve("2(a3(b))"));     
-console.log(solve("3(a2(bc))"));    
-console.log(solve("2(abc)3(cd)ef")); 
+console.log(solve("3(ab)"));
+console.log(solve("2(a3(b))"));
 
