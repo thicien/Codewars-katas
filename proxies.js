ba4fb433214aaa021d00c6b513846f8728988568
mugisha thicien <mugishathicien04@gmail.com>
2025-10-16 17:31:42 +0200

    security has placed

--- proxies.js
+++ proxies.js
@@ -13,4 +13,20 @@ const secure = new Proxy(confidential, {
   }
 });
 
-console.log(secure.password); 
+console.log(secure.password);
+//salary encryption
+const boss = {
+  name: "Mr. Thicien",
+  salary: 50000
+};
+
+const assistant = new Proxy(boss, {
+  get(target, prop) {
+    if (prop === "salary") {
+      return "These information are confidential";
+    }
+    return target[prop];
+  }
+})
+console.log(assistant.name);
+console.log(assistant.salary);
