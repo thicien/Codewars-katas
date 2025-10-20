23d1c82f3ae275fce2bcdd44280deaa8cc74f408
mugisha thicien <mugishathicien04@gmail.com>
2025-10-20 16:48:08 +0200

    sum of powers calculated

--- kata.js
+++ kata.js
@@ -1,14 +1,29 @@
-function digitSum(x) {
-  return x.toString().split('').reduce((s, d) => s + Number(d), 0);
+// numbers that are powers of their sum of digits 
+function powerSumDigTerm(n) {
+  const results = [];
+
+  for (let s = 2; s < 200; s++) { 
+    let power = 2;
+    while (power < 20) { 
+      let num = Math.pow(s, power);
+
+      if (num < 10) { 
+        power++;
+        continue;
+      }
+
+      const sum = num
+        .toString()
+        .split('')
+        .reduce((acc, d) => acc + Number(d), 0);
+
+      if (sum === s) results.push(num);
+
+      power++;
+    }
+  }
+
+  results.sort((a, b) => a - b);
+  return results[n - 1];
 }
 
-function solve(n) {
-  let s = n.toString();
-  let len = s.length;
-  let a = Number('9'.repeat(len - 1));
-  let b = n - a;
-  return digitSum(a) + digitSum(b);
-}
-console.log(solve(29));   
-console.log(solve(100));  
-console.log(solve(483));  
