636f8ddaff797932eea67d71ede3948e550b6dea
mugisha thicien <mugishathicien04@gmail.com>
2025-10-20 16:45:09 +0200

    changed

--- katas.js
+++ katas.js
@@ -1,13 +1,20 @@
-function makeLooper(str) {
-  let i = 0;
-  return function() {
-    const ch = str[i];
-    i = (i + 1) % str.length;
-    return ch;
-  };
+// subsets 
+function subsetSum(xs, target) {
+  if (target === 0) return [];
+  const sums = new Map();
+  sums.set(0, []); 
+  for (const num of xs) {
+    const newSums = new Map(sums);
+
+    for (const [s, subset] of sums.entries()) {
+      const newSum = s + num;
+      if (newSum === target) return [...subset, num];
+      if (newSum < target && !newSums.has(newSum)) {
+        newSums.set(newSum, [...subset, num]);
+      }
+    }
+    for (const [k, v] of newSums) sums.set(k, v);
+  }
+
+  return null;
 }
-const abc = makeLooper('abc');
-console.log(abc()); 
-console.log(abc()); 
-console.log(abc()); 
-console.log(abc());
