ccd400e474d416c457f5e5d50ba3f77b6f9957e0
mugisha thicien <mugishathicien04@gmail.com>
2025-10-28 10:49:04 +0200

    problem solved well

--- namingNumbers.js
+++ namingNumbers.js
@@ -1,52 +1,57 @@
-function numberToWords(num) {
+function numberToEnglish(num) {
+  
   if (!Number.isInteger(num) || num < 0 || num > 99999) return "";
-
-  const ones = [
-    "zero", "one", "two", "three", "four",
-    "five", "six", "seven", "eight", "nine",
-    "ten", "eleven", "twelve", "thirteen", "fourteen",
-    "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
-  ];
-
-  const tens = [
-    "", "", "twenty", "thirty", "forty",
-    "fifty", "sixty", "seventy", "eighty", "ninety"
-  ];
-
-  function convertBelowThousand(n) {
-    let result = "";
-    if (n >= 100) {
-      result += ones[Math.floor(n / 100)] + " hundred";
-      n %= 100;
-      if (n > 0) result += " ";
-    }
-    if (n >= 20) {
-      result += tens[Math.floor(n / 10)];
-      if (n % 10 !== 0) result += " " + ones[n % 10];
-    } else if (n > 0) {
-      result += ones[n];
-    }
-    return result;
-  }
-
   if (num === 0) return "zero";
 
-  let words = "";
+  const ones = [
+    "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+  ];
+  const teens = [ "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+                 "sixteen", "seventeen", "eighteen","nineteen",
+  ];
+  
+  const tens = [
+    "",
+    "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety",
+  ];
 
-  if (num >= 1000) {
-    const thousands = Math.floor(num / 1000);
-    words += convertBelowThousand(thousands) + " thousand";
-    const remainder = num % 1000;
-    if (remainder > 0) words += " " + convertBelowThousand(remainder);
-  } else {
-    words += convertBelowThousand(num);
+    function convertHundreds(n) {
+    let words = "";
+
+    if (n >= 100) {
+      words += ones[Math.floor(n / 100)] + " hundred";
+      n %= 100;
+      if (n > 0) words += " ";
+    }
+
+    if (n >= 20) {
+      words += tens[Math.floor(n / 10)];
+      n %= 10;
+      if (n > 0) words += " " + ones[n];
+    } else if (n >= 10) {
+      words += teens[n - 10];
+    } else if (n > 0) {
+      words += ones[n];
+    }
+    return words.trim();
   }
+  const thousands = Math.floor(num / 1000);
+  const remainder = num % 1000;
 
-  return words.trim();
+  let result = "";
+
+  if (thousands > 0) {
+    result += convertHundreds(thousands) + " thousand";
+    if (remainder > 0) result += " ";
+  }
+  if (remainder > 0) {
+    result += convertHundreds(remainder);
+  }
+  return result.trim();
 }
-console.log(numberToWords(0));       // "zero"
-console.log(numberToWords(27));      // "twenty seven"
-console.log(numberToWords(100));     // "one hundred"
-console.log(numberToWords(7012));    // "seven thousand twelve"
-console.log(numberToWords(99205));   // "ninety nine thousand two hundred five"
-console.log(numberToWords(123456));  // "" (out of range)
+console.log(numberToEnglish(0));
+console.log(numberToEnglish(45);
+console.log(numberToEnglish(3476));
+console.log(numberToEnglish(32987));
+console.log(numberToEnglish(20098));
+console.log(numberToEnglish(1000));
