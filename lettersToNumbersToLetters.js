aca68c8be739bc4f7f48480ca9861ccc83b7845b
mugisha thicien <mugishathicien04@gmail.com>
2025-11-05 16:19:32 +0200

    numbers of letters

--- lettersToNumbersToLetters.js
+++ lettersToNumbersToLetters.js
@@ -1,3 +1,22 @@
-function numberToLetters() {
-    
+function numberToLetters(n) {
+    const numbers = ['zero','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    function toWord(num) {
+        if (num === 0) return 'zero';
+        return String(num).split('').map(digits => numbers[Number(digits)]).join('');
+    }
+    const result = [];
+    let current = n;
+    while (true) {
+        const word = toWord(current);
+        result.push(word);
+        const nextNumber = word.length;
+        const nextWord = toWord(nextNumber);
+        if (nextWord === word) continue;
+        current = nextNumber;
+    }
+    return result;
 }
+console.log(numbersOfLetters(65));
+console.log(numbersOfLetters(4));
+console.log(numbersOfLetters(8));
+
