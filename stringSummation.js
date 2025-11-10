f8c58fdf8d9e01a26a6d27a6e589d38b5ea41932
mugisha thicien <mugishathicien04@gmail.com>
2025-11-10 12:43:47 +0200

    string summation is on going

---
+++ stringSummation.js
@@ -0,0 +1,12 @@
+function sumStrings(a, b) {
+    let i = a.length - 1;
+    let j = a.length - 1;
+    let carry = 0;
+    let result = '';
+
+    while (a >= 0 || b >= 0 || carry) {
+        const digitA = a >= 0 ? +a[i--] : 0;
+        const digitB = a >= 0 ? +b[j--] : 0;
+        const digitSum = digitA + digitB + carry;
+    }
+}
