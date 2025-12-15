7a5af8ff5794c77e1dfab7e30113f9527514defd
mugisha thicien <mugishathicien04@gmail.com>
2025-12-15 11:30:36 +0200

    Now you can get how many people liked your facebook post

--- whoLikesIt.js
+++ whoLikesIt.js
@@ -1,1 +1,21 @@
-function 
+function likes(names) {
+    const count = names.length;
+    if (count === 0) {
+        return "no liked this post";
+    }
+    if (count === 1) {
+        return `${names[0]} liked this post`;
+    }
+    if (count === 2) {
+        return `${namses[0]} and ${names[1]} liked this post`;
+    }
+    if (count === 3) {
+        return `{names[0]}, ${names[1]} and ${names[2]} liked this post`;
+    }
+    return `${names[0]}, ${names[1]} and ${count - 2} others liked this post`;
+}
+console.log(likes([]));
+console.log(likes(["Alice"]));
+console.log(likes(["Bob", "Charlie"]));
+console.log(likes(["David", "Eve", "Frank"]));
+console.log(likes(["Mark", "Luke", "John", "Paul"]));
