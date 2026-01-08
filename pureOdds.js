32abc754dfa58bd7e8a6ffdcddf7c3b4d341af57
mugisha thicien <mugishathicien04@gmail.com>
2026-01-08 14:43:30 +0200

    Create onlyOddDigitPrimes

---
+++ pureOdds.js
@@ -0,0 +1,42 @@
+function onlyOddDigPrimes(n) {
+  function isPrime(num) {
+    if (num < 2) return false;
+    for (let i = 2; i * i <= num; i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  }
+
+  // Check if all digits are odd
+  function hasOnlyOddDigits(num) {
+    return num
+      .toString()
+      .split('')
+      .every(digit => digit % 2 !== 0);
+  }
+
+  let count = 0;
+  let largestBelow = null;
+
+  // Count and find largest ≤ n
+  for (let i = 2; i <= n; i++) {
+    if (isPrime(i) && hasOnlyOddDigits(i)) {
+      count++;
+      largestBelow = i;
+    }
+  }
+
+  // Find smallest > n
+  let smallestAbove = n + 1;
+  while (true) {
+    if (isPrime(smallestAbove) && hasOnlyOddDigits(smallestAbove)) {
+      break;
+    }
+    smallestAbove++;
+  }
+
+  return [count, largestBelow, smallestAbove];
+}
+console.log(onlyOddDigPrimes(20)); // [7, 19, 31]
+console.log(onlyOddDigPrimes(40)); // [9, 37, 53]
+console.log(onlyOddDigPrimes(11)); // [5, 11, 13]
