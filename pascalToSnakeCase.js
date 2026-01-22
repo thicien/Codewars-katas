f0b8055a20d8fd32ffb617b750bf612a481f0153
mugisha thicien <mugishathicien04@gmail.com>
2026-01-22 10:20:56 +0200

    Conversion of pascal's case into snake's case

--- pascalToSnakeCase.js
+++ pascalToSnakeCase.js
@@ -1,1 +1,10 @@
-function 
+function toUnderscore(input) {
+    if (typeof input === 'number') {
+        return input.toString();
+    }
+    return input.replace(/([A - Z])/g, '_$1').toLowerCase().replace(/^_/, '');
+}
+console.log(toUnderscore("PascalCase"));
+console.log(toUnderscore("AnotherExampleHere"));
+console.log(toUnderscore("Test"));
+console.log(toUnderscore(12345));
