217ff517d38340aafeff5476eb5576a663dca660
mugisha thicien <mugishathicien04@gmail.com>
2026-01-29 17:54:25 +0200

    Working on two some integers

---
+++ twoSumIntegers.js
@@ -0,0 +1,13 @@
+function Add(a, b) {
+    while (b !== 0) {
+        let carry = (a & b) << 1;
+        a = a ^ b;
+        b = carry;
+    }
+    return a;
+}
+console.log(Add(5, 19));     
+console.log(Add(-27, 18)); 
+console.log(Add(-14, -16));  
+console.log(Add(0, 0));    
+console.log(Add(10, -10)); 
