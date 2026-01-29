1eeedc696235705482f7f073fe9f22d863cc4eca
mugisha thicien <mugishathicien04@gmail.com>
2026-01-29 17:57:09 +0200

    Applying the bitwise operators to get a sum of two numbers

--- twoSumIntegers.js
+++ twoSumIntegers.js
@@ -10,4 +10,5 @@ console.log(Add(5, 19));
 console.log(Add(-27, 18)); 
 console.log(Add(-14, -16));  
 console.log(Add(0, 0));    
-console.log(Add(10, -10)); 
+console.log(Add(10, -10));
+console.log(Add(123456789, 987654321));
