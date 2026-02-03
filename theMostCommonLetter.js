51a10f9f2b436f34e0e180a05fcfb49abe586168
mugisha thicien <mugishathicien04@gmail.com>
2026-02-03 15:58:26 +0200

    Implementing the correct login for getting the common letter in a sentence

---
+++ theMostCommonLetter.js
@@ -0,0 +1,29 @@
+function replaceCommon(string, letter) {
+  //good luck
+    let count = {}
+    let findIndex = {}
+    for(let i = 0; i < string.length; i++) {
+        let character = string[i];
+        if(character !== " ") {
+            count[character] = (count[character] || 0) + 1;
+            if(findIndex[character] === undefined) {
+                findIndex[character] = i;
+            }
+        }
+    }
+    let mostCommon = "";
+    let maxCommon = 0;
+    let earliestIndex = Infinity;
+    for(let character in count) {
+       if(count[character] > maxCommon || count[character] === maxCommon && findIndex[character] < earliestIndex) {
+           mostCommon = character;
+           maxCommon = count[character];
+           earliestIndex = findIndex[character];
+       }   
+    }
+    return string.split(mostCommon).join(letter);
+}
+
+console.log(replaceCommon('my mom loves me as never did', 't'));
+console.log(replaceCommon('real talk bro', 'n'));
+console.log(replaceCommon('great job go ahead', 'k'));
